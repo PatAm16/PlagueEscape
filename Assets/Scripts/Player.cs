cd6a1862799f8caff07e9addc13eef49e9310aac
PatAm16 <patricia.amaral16@gmail.com>
2021-05-05 10:15:15 +0100

    Update PlagueEscape.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Jogador : MonoBehaviour
 {
     [SerializeField]
     private float movespeed = 5f;
