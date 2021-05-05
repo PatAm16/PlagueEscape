31a7d0c93618a0cc3f02ddcb3bbe5da22114f083
MarianaCidade <79975190+MarianaCidade@users.noreply.github.com>
2021-05-05 11:33:26 +0100

    Update_Versao

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jogador : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField]
     private float movespeed = 5f;
