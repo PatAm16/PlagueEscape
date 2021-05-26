a9eea8e1d9cdfb3019009efb512bfec9bf08cab4
MarianaCidade <79975190+MarianaCidade@users.noreply.github.com>
2021-05-26 15:50:47 +0100

    Update_ScriptGameOver

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject MenuGameOver;
+
+    bool jogoAcabou = false;
+
+    [SerializeField]
+    float tempoDeEspera = 2f;
+
+    float tempoDecorrido = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        MenuGameOver.SetActive(false);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jogoAcabou == false)
+        {
+            tempoDecorrido += Time.deltaTime;
+            if (tempoDecorrido >= tempoDeEspera)
+            {
+                MenuGameOver.SetActive(true);
+                jogoAcabou = true;
+            }
+        }
+        
+    }
+
+   
+}
