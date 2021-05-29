f2e11644084c03065a6c27b534ed48c69fd7c3e1
MarianaCidade <79975190+MarianaCidade@users.noreply.github.com>
2021-05-29 18:17:50 +0100

    Update_Som

---
+++ Assets/Scripts/SomPassos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SomPassos : MonoBehaviour
+{
+    public AudioClip PassosSom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(PassosSom, transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
